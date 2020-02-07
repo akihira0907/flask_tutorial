7ced389769f06dfee471a1eb0ee368cac0213801
sororne <sororddr@yahoo.co.jp>
2020-02-08 04:22:06 +0900

    配列の変数名を変更

--- app/app.py
+++ app/app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     name = request.args.get("name")
-    weapons = ["sword", "gun", "shield", "bow"]
-    return render_template("index.html", name=name, weapons=weapons)
+    oretsuba_names = ["hiyoko", "asuka", "naru", "miyako"]
+    return render_template("index.html", name=name, oretsuba_names=oretsuba_names)
 
 
 @app.route("/index", methods=["post"])
 def post():
     name = request.form["name"]
-    weapons = ["sword", "gun", "shield", "bow"]
-    return render_template("index.html", name=name, weapons=weapons)
+    oretsuba_names = ["hiyoko", "asuka", "naru", "miyako"]
+    return render_template("index.html", name=name, oretsuba_names=oretsuba_names)
 
 
 if __name__ == "__main__":
